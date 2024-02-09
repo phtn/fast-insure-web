0b7046e96306da634c59d56f7d55de23d583e9c7
Jun Orville Lecena <jun.lecena@comptrolla.com>
2024-02-09 13:28:06 +0800

    update: tailwind config

--- src/app/_components/kerrigan/raynor.tsx
+++ src/app/_components/kerrigan/raynor.tsx
@@ -1,8 +1,8 @@
-'use client'
+"use client";
 
 import { Button } from "@@components/button";
 import { opts } from "@@utils/helpers";
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 import { ArrowUpRightIcon, LayoutGridIcon } from "lucide-react";
 import Image from "next/image";
 import { useCallback } from "react";
@@ -12,23 +12,23 @@ import Link from "next/link";
 
 const RaynorList = ({ data, title }: KerriganListProps) => {
   return (
-    <section className="px-6 md:px-16 pb-10 ">
-      <h2 className="text-3xl font-extrabold mb-4">{title}</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+    <section className="px-6 pb-10 md:px-16 ">
+      <h2 className="mb-4 text-3xl font-extrabold">{title}</h2>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
         {data?.map((item) => (
           <div
             key={item?.id}
-            className="border rounded-lg overflow-hidden bg-background dark:bg-indigo-100"
+            className="bg-background overflow-hidden rounded-lg border dark:bg-indigo-100"
           >
             <Image
               alt={item.src ?? ""}
-              className="w-full h-64 object-cover"
+              className="h-64 w-full object-cover"
               src={item?.src ?? "/logo/fi_logo_v1.svg"}
               width={300}
               height={300}
             />
             <div className="p-4">
-              <h3 className="font-bold text-lg">{item.title}</h3>
+              <h3 className="text-lg font-bold">{item.title}</h3>
               <p className="text-gray-500">{item.description}</p>
               <Button className="mt-2">
                 {item.subtext ?? item.description}
@@ -47,76 +47,78 @@ export const Raynor = ({
   data,
   listTitle,
   actionLabel,
-  href
+  href,
 }: KerriganProps) => {
   const List = useCallback(() => {
     const withData = typeof data !== "undefined";
 
-    const options = opts(
-      <RaynorList data={data} title={listTitle} />,
-      <div />,
-    );
+    const options = opts(<RaynorList data={data} title={listTitle} />, <div />);
     return <>{options.get(withData)}</>;
   }, [data, listTitle]);
   return (
     <main>
       <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
         <div className="px-4 md:px-6">
-          <div className="flex flex-col justify-center items-center space-y-8 text-center">
-            <div className="space-y-8 md:space-y-6 flex flex-col items-center ">
-              <motion.div initial={{ x: -50, opacity: 0 }}
+          <div className="flex flex-col items-center justify-center space-y-8 text-center">
+            <div className="flex flex-col items-center space-y-8 md:space-y-6 ">
+              <motion.div
+                initial={{ x: -50, opacity: 0 }}
                 animate={{ x: 0, opacity: 1 }}
                 transition={{
                   duration: 0.5,
                   delay: 3.3,
-                  easings: ['easeInOut']
-                }}>
-                <Title>
-                  {title}
-                </Title>
+                  easings: ["easeInOut"],
+                }}
+              >
+                <Title>{title}</Title>
               </motion.div>
-              <motion.div initial={{ x: -30, opacity: 0 }}
+              <motion.div
+                initial={{ x: -30, opacity: 0 }}
                 animate={{ x: 0, opacity: 1 }}
                 transition={{
                   duration: 0.2,
                   delay: 3.6,
-                  easings: ['easeInOut']
-                }}>
-                <Description>
-                  {description}
-                </Description>
+                  easings: ["easeInOut"],
+                }}
+              >
+                <Description>{description}</Description>
               </motion.div>
             </div>
-            <div className="md:w-full w-[calc(100vw-64px)] flex items-center justify-center space-x-4">
-              <motion.div initial={{ y: 10, scale: 0.5, opacity: 0 }}
+            <div className="flex w-[calc(100vw-64px)] items-center justify-center space-x-4 md:w-full">
+              <motion.div
+                initial={{ y: 10, scale: 0.5, opacity: 0 }}
                 animate={{ y: 0, scale: 1, opacity: 1 }}
                 transition={{
                   duration: 0.3,
                   delay: 3.5,
-                  easings: ['easeInOut']
-                }}>
+                  easings: ["easeInOut"],
+                }}
+              >
                 <Link href={href ?? `/`}>
-                  <button className="inline-flex h-[50px] text-[14px] md:text-lg md:w-[350px] group hover:scale-[105%] transition-all duration-[5000] font-bold animate-shimmer items-center justify-center rounded-lg border border-blue-300 bg-[linear-gradient(110deg,#000103,45%,#93c5fd,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-white focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+                  <button className="duration-[4000] group inline-flex h-[50px] animate-shimmer items-center justify-center rounded-lg border border-blue-300 bg-[linear-gradient(110deg,#000103,45%,#93c5fd,55%,#000103)] bg-[length:200%_100%] px-6 text-[14px] font-bold text-white transition-all hover:scale-[105%] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 md:w-[350px] md:text-lg">
                     {actionLabel}
-                    <LayoutGridIcon className="h-5 w-5 ml-6 isolate group-hover:text-blue-300 transition-all duration-300" />
+                    <LayoutGridIcon className="isolate ml-6 h-5 w-5 transition-all duration-300 group-hover:text-blue-300" />
                   </button>
                 </Link>
               </motion.div>
-              <motion.div initial={{ y: 10, scale: 0.5, opacity: 0 }}
+              <motion.div
+                initial={{ y: 10, scale: 0.5, opacity: 0 }}
                 animate={{ y: 0, scale: 1, opacity: 1 }}
                 transition={{
                   duration: 0.3,
                   delay: 3.6,
-                  easings: ['easeInOut']
-                }}>
-                <Button size='lg' variant='outline' className="hidden min-w-[200px] space-x-4 md:flex hover:scale-[105%] transition-all duration-500">
+                  easings: ["easeInOut"],
+                }}
+              >
+                <Button
+                  size="lg"
+                  variant="outline"
+                  className="hidden min-w-[200px] space-x-4 transition-all duration-500 hover:scale-[105%] md:flex"
+                >
                   <span>Sign up</span>
                   <ArrowUpRightIcon className="h-4 w-4" />
                 </Button>
               </motion.div>
-
-
-
             </div>
           </div>
         </div>
@@ -130,8 +132,8 @@ const Title = tw.h2`
   text-[2.5rem] leading-[2.5rem] md:text-[2.75rem] md:leading-[2.75rem] max-w-[10ch] md:max-w-[32ch] font-extrabold tracking-tighter 
   text-transparent bg-clip-text bg-gradient-to-r from-blue-950 from-20% to-blue-400
   sm:text-4xl md:text-5xl lg:text-6xl/none h-fit py-1 
-`
+`;
 
 const Description = tw.h3`
   mx-auto max-w-[28ch] py-1 md:max-w-[48ch] text-blue-950 text-[14px] font-medium md:text-xl
-`
+`;
--- src/app/affiliate.tsx
+++ src/app/affiliate.tsx
@@ -1,39 +1,67 @@
-'use client'
-import { motion } from 'framer-motion'
-import { ArrowUpRightIcon, ArrowUpRightSquare, SatelliteIcon } from 'lucide-react'
-import Image from 'next/image'
-import { Button } from './_components/button'
+"use client";
+
+import { motion } from "framer-motion";
+import {
+  ArrowUpRightIcon,
+  ArrowUpRightSquare,
+  SatelliteIcon,
+} from "lucide-react";
+import Image from "next/image";
+import { Button } from "./_components/button";
 
 export const Affiliate = () => {
   return (
-    <div className={`bg-blue-400 pt-[100px] md:pt-[200px] overflow-clip`}>
-      <motion.div initial={{ x: `calc(-50vw)`, y: -50, scale: 0.1 }} animate={{ x: `calc(110vw)`, y: 75, scale: 1, rotate: `15deg` }} transition={{ delay: 5, duration: 120, easings: ['easeIn'], repeat: 10 }} className="flex w-full animate-shimmer">
-        <Image src={`/svg/sat_v1.svg`} width={100} height={100} alt="satellite" className="h-2 w-2 md:h-5 md:w-5 opacity-50 text-orange-50/50" />
+    <div className={`overflow-clip bg-blue-400 pt-[100px] md:pt-[200px]`}>
+      <motion.div
+        initial={{ x: `calc(-50vw)`, y: -50, scale: 0.1 }}
+        animate={{ x: `calc(110vw)`, y: 75, scale: 1, rotate: `15deg` }}
+        transition={{
+          delay: 5,
+          duration: 120,
+          easings: ["easeIn"],
+          repeat: 10,
+        }}
+        className="flex w-full animate-shimmer"
+      >
+        <Image
+          src={`/svg/sat_v1.svg`}
+          width={100}
+          height={100}
+          alt="satellite"
+          className="h-2 w-2 text-orange-50/50 opacity-50 md:h-5 md:w-5"
+        />
       </motion.div>
-      <motion.div initial={{ x: -2000, y: -30, scale: 0.25 }} animate={{ x: 2000, y: `calc(100vw/12)`, scale: 1.5 }} transition={{ duration: 40, easings: ['easeIn'], repeat: 10 }} className="flex w-full">
-        <SatelliteIcon className="h-2 w-2 md:h-5 md:w-5 text-orange-50/80" />
+      <motion.div
+        initial={{ x: -2000, y: -30, scale: 0.25 }}
+        animate={{ x: 2000, y: `calc(100vw/12)`, scale: 1.5 }}
+        transition={{ duration: 40, easings: ["easeIn"], repeat: 10 }}
+        className="flex w-full"
+      >
+        <SatelliteIcon className="h-2 w-2 text-orange-50/80 md:h-5 md:w-5" />
       </motion.div>
-      <div className='h-[100px] md:h-[200px] flex flex-col items-center justify-center'>
-        <h4 className='text-[1.75rem] md:text-[3.5rem] my-2 text-transparent bg-clip-text bg-gradient-to-br from-white to-orange-50 font-extrabold tracking-tighter'>
+      <div className="flex h-[100px] flex-col items-center justify-center md:h-[200px]">
+        <h4 className="my-2 bg-gradient-to-br from-white to-orange-50 bg-clip-text text-[1.75rem] font-extrabold tracking-tighter text-transparent md:text-[3.5rem]">
           Join our Affiliate Program.
         </h4>
-        <div className='flex space-x-4'>
-          <button className="inline-flex h-[50px] text-[16px] md:text-lg w-[250px] md:w-[300px] group hover:scale-[105%] transition-all duration-[4000] font-bold animate-shimmer items-center justify-center rounded-lg border border-blue-300 bg-[linear-gradient(110deg,#000103,45%,#93c5fd,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-white focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+        <div className="flex space-x-4">
+          <button className="duration-[4000] group inline-flex h-[50px] w-[250px] animate-shimmer items-center justify-center rounded-lg border border-blue-300 bg-[linear-gradient(110deg,#000103,45%,#93c5fd,55%,#000103)] bg-[length:200%_100%] px-6 text-[16px] font-bold text-white transition-all hover:scale-[105%] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 md:w-[300px] md:text-lg">
             Sign up today!
-            <ArrowUpRightSquare className="h-5 w-5 ml-6 isolate group-hover:text-blue-300 transition-all duration-300" />
+            <ArrowUpRightSquare className="isolate ml-6 h-5 w-5 transition-all duration-300 group-hover:text-blue-300" />
           </button>
-          <Button size='lg' variant='outline' className="hidden min-w-[200px] rounded-lg md:flex h-12 hover:scale-[105%] transition-all duration-500">
+          <Button
+            size="lg"
+            variant="outline"
+            className="hidden h-12 min-w-[200px] rounded-lg transition-all duration-500 hover:scale-[105%] md:flex"
+          >
             How it works?
-            <ArrowUpRightIcon className="h-5 w-5 ml-6 isolate group-hover:text-blue-300 transition-all duration-300" />
+            <ArrowUpRightIcon className="isolate ml-6 h-5 w-5 transition-all duration-300 group-hover:text-blue-300" />
           </Button>
         </div>
       </div>
 
-      <div className="md:h-[650px] h-[150px] bg-[url('/bg/city_v2.webp')] border-0 bg-contain bg-center md:bg-cover bg-no-repeat md:bg-top">
-        <div className="h-full bg-gradient-to-b from-blue-400 from-5% to-transparent to-100%">
-
-        </div>
+      <div className="h-[150px] border-0 bg-[url('/bg/city_v2.webp')] bg-contain bg-center bg-no-repeat md:h-[650px] md:bg-cover md:bg-top">
+        <div className="h-full bg-gradient-to-b from-blue-400 from-5% to-transparent to-100%"></div>
       </div>
     </div>
-  )
-}
+  );
+};
