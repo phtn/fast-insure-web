6c461f10d9bfd86be17f7b455a8f57217fd5b0be
Jun Orville Lecena <jun.lecena@comptrolla.com>
2024-02-09 13:06:35 +0800

    fix: convert_to_pdf set to optional

--- src/server/ocr/ocr.ts
+++ src/server/ocr/ocr.ts
@@ -1,12 +1,18 @@
 import axios, { type AxiosRequestConfig } from "axios";
-import type { OCRSchema, OCR_Data_Schema, OCR_GOOG_Schema } from "@resource/ocr";
+import type {
+  OCRSchema,
+  OCR_Data_Schema,
+  OCR_GOOG_Schema,
+} from "@resource/ocr";
 
 const onExtraction = async (options: AxiosRequestConfig) => {
-  return await axios.request(options).then((res: { data: OCR_GOOG_Schema }) => res).catch((err: Error) => err)
-}
+  return await axios
+    .request(options)
+    .then((res: { data: OCR_GOOG_Schema }) => res)
+    .catch((err: Error) => err);
+};
 
 export const runOCR = async (values: OCRSchema) => {
-
   const data: OCR_Data_Schema = {
     response_as_dict: true,
     attibutes_as_list: true,
@@ -14,18 +20,18 @@ export const runOCR = async (values: OCRSchema) => {
     providers: "google",
     file_url: values.file_url,
     fallback_providers: "amazon",
-  }
+  };
 
   const options = {
-    method: 'POST',
-    url: 'https://api.edenai.run/v2/ocr/ocr',
+    method: "POST",
+    url: "https://api.edenai.run/v2/ocr/ocr",
     headers: {
-      accept: 'application/json',
-      'content-type': 'application/json',
-      authorization: `Bearer ${process.env.EDEN_OCR_KEY}`
+      accept: "application/json",
+      "content-type": "application/json",
+      authorization: `Bearer ${process.env.EDEN_OCR_KEY}`,
     },
     data,
-  }
+  };
 
-  return await onExtraction(options)
-} 
+  return await onExtraction(options);
+};
--- src/server/resource/ocr.ts
+++ src/server/resource/ocr.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const OCRResource = z.object({
-  file_url: z.string()
-})
+  file_url: z.string(),
+});
 
 export const OCR_MSFT_Response = z.object({
   microsoft: z.object({
@@ -14,12 +14,12 @@ export const OCR_MSFT_Response = z.object({
         left: z.number(),
         top: z.number(),
         width: z.number(),
-        height: z.number()
-      })
+        height: z.number(),
+      }),
     ),
-    cost: z.number()
-  })
-})
+    cost: z.number(),
+  }),
+});
 
 export const OCR_GOOG_Response = z.object({
   google: z.object({
@@ -31,12 +31,12 @@ export const OCR_GOOG_Response = z.object({
         left: z.number(),
         top: z.number(),
         width: z.number(),
-        height: z.number()
-      })
+        height: z.number(),
+      }),
     ),
-    cost: z.number()
-  })
-})
+    cost: z.number(),
+  }),
+});
 
 export const OCR_AMZN_Response = z.object({
   amazon: z.object({
@@ -48,58 +48,61 @@ export const OCR_AMZN_Response = z.object({
         left: z.number(),
         top: z.number(),
         width: z.number(),
-        height: z.number()
-      })
+        height: z.number(),
+      }),
     ),
-    cost: z.number()
-  })
-})
+    cost: z.number(),
+  }),
+});
 
 export const Providers = z.union([
-  z.literal('microsoft'),
-  z.literal('google'),
-  z.literal('amazon'),
-  z.literal('base64'),
-])
+  z.literal("microsoft"),
+  z.literal("google"),
+  z.literal("amazon"),
+  z.literal("base64"),
+]);
 
 export const OCR_Data = z.object({
   response_as_dict: z.boolean(),
   attibutes_as_list: z.boolean(),
   show_original_response: z.boolean(),
-  convert_to_pdf: z.boolean(),
+  convert_to_pdf: z.boolean().optional(),
   language: z.string().optional(),
   providers: Providers,
   file_url: z.string(),
-  fallback_providers: Providers.optional()
-})
+  fallback_providers: Providers.optional(),
+});
 
-export const Fields = z.array(z.object({
-  key: z.string(),
-  value: z.string(),
-  bounding_box: z.array(z.object({
-    left: z.number(),
-    top: z.number(),
-    width: z.number(),
-    height: z.number(),
-  })),
-  confidence_score: z.number(),
-
-}))
+export const Fields = z.array(
+  z.object({
+    key: z.string(),
+    value: z.string(),
+    bounding_box: z.array(
+      z.object({
+        left: z.number(),
+        top: z.number(),
+        width: z.number(),
+        height: z.number(),
+      }),
+    ),
+    confidence_score: z.number(),
+  }),
+);
 
 export const OCR_DE_BASE64_Resource = z.object({
   base64: z.object({
     status: z.string(),
     fields: Fields,
-    cost: z.number()
-  })
-})
+    cost: z.number(),
+  }),
+});
 
-export type OCRSchema = z.infer<typeof OCRResource>
-export type OCR_MSFT_Schema = z.infer<typeof OCR_MSFT_Response>
-export type OCR_GOOG_Schema = z.infer<typeof OCR_GOOG_Response>
-export type OCR_AMZN_Schema = z.infer<typeof OCR_AMZN_Response>
-export type OCR_Data_Schema = z.infer<typeof OCR_Data>
-export type OCR_DE_BASE64_Schema = z.infer<typeof OCR_DE_BASE64_Resource>
-export type OCR_DE_FieldSchema = z.infer<typeof Fields>
+export type OCRSchema = z.infer<typeof OCRResource>;
+export type OCR_MSFT_Schema = z.infer<typeof OCR_MSFT_Response>;
+export type OCR_GOOG_Schema = z.infer<typeof OCR_GOOG_Response>;
+export type OCR_AMZN_Schema = z.infer<typeof OCR_AMZN_Response>;
+export type OCR_Data_Schema = z.infer<typeof OCR_Data>;
+export type OCR_DE_BASE64_Schema = z.infer<typeof OCR_DE_BASE64_Resource>;
+export type OCR_DE_FieldSchema = z.infer<typeof Fields>;
 
-export const ocrUrl = `url: "/ocr/ocr"`
+export const ocrUrl = `url: "/ocr/ocr"`;
